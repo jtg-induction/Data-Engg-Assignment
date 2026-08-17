b77893a0ba9e8e018e320c5f406ed1116d286892
Anurag sharma <anurag.sharma@joshtechnologygroup.com>
2026-08-17 13:05:51 +0530

    fixed the tire filtering for parts and logic in question 7

--- Question 9/query.sql
+++ Question 9/query.sql
@@ -1,20 +1,33 @@
 -- Q9 Top 10 Tires Bonus: Top 10 dealer of nation will get $3 per tire additional incentive
-
+-- cost time = 6.5s
+WITH dealers_with_their_rank AS (
+    SELECT
+        s.dealernumber AS dealernumber,
+        to_char(s.calendardate :: date, 'YYYY FMMonth') AS month_of_sale,
+        SUM(s.units) AS total_units_sold,
+        ROW_NUMBER() OVER(
+            PARTITION BY to_char(s.calendardate :: date, 'YYYY FMMonth')
+            ORDER BY
+                SUM(s.units) DESC
+        ) AS dealers_rank
+    FROM
+        sales s
+        INNER JOIN parts p ON s.part_number = p.part_number
+    WHERE
+        p.part_category_2 ILIKE '%tires%'
+    GROUP BY
+        to_char(s.calendardate :: date, 'YYYY FMMonth'),
+        s.dealernumber
+)
 SELECT
-    s.dealernumber,
-    to_char(s.calendardate::date,'YYYY FMMonth') AS month_of_sale,
-    Sum(s.units) * 3 AS ties_incentive
+    month_of_sale,
+    dealernumber,
+    total_units_sold,
+    total_units_sold * 3 AS addtional_top_10_incentive
 FROM
-    sales s     
-INNER JOIN
-    parts p ON s.part_number = p.part_number
+    dealers_with_their_rank
 WHERE
-    p.part_category_2 ILIKE '%tires%'
-GROUP BY
-    s.dealernumber,
-    to_char(s.calendardate::date,'YYYY FMMonth')
+    dealers_rank <= 10
 ORDER BY
-    SUM(s.units) DESC    
-LIMIT 10    
-
-
+    month_of_sale ASC,
+    total_units_sold DESC
